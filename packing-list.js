eefc490186f1307ad86ec1868e0d4547e04d3db1
Justin Mulroney <your@email.comjustinmulroney@gmail.com>
2022-01-17 21:55:22 -0500

    commiting packing while loop

--- packing-list.js
+++ packing-list.js
@@ -1,4 +1,7 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
-for(i = 0;i < packingList.length; i++){
+console.log('Kitchen stuff to pack');
+let i = 0;
+while(i<packingList.length){
   console.log(packingList[i]);
+  i++;
 }
